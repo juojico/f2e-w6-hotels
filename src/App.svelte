e4545408683a9e7dc87b4c883e7624cbc55d89b2
jico <u4m062@gmail.com>
2019-08-21 22:43:58 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import ImgBox from './components/ImgBox/ImgBox.svelte'
+	export let name;
+</script>
+
+<style type="text/scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+  }
+
+  div {
+    background: green;
+
+    > p {
+      color: #fff;
+    }
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+
+
+<div>
+  <p>SASS is working!</p>
+</div>
+
+<ImgBox/>
---
+++ src/components/ImgBox/ImgBox.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+
+<div>imgBox</div>
